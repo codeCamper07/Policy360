c2620cf9d6d8676bf81460f66039cd94c1b057ee
Bhargav Dommaraju <dommarajubhargav@Bhargavs-MacBook-Air.local>
2025-10-30 19:13:45 +0530

    neon change

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 
+import { PrismaNeon } from '@prisma/adapter-neon'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const connectionString = `${process.env.DATABASE_URL}`
+const adapter = new PrismaNeon({ connectionString })
+
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
