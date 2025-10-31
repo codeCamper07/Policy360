9501c96bbe32605199412dfa0b197e0fbda9e443
Bhargav Dommaraju <dommarajubhargav@Bhargavs-MacBook-Air.local>
2025-10-31 18:06:31 +0530

    changes on table search

--- components/tableSearch.tsx
+++ components/tableSearch.tsx
@@ -6,7 +6,7 @@ const TableSearch = () => {
   const router = useRouter()
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    const value = e.currentTarget[0].value
+    const value = (e.currentTarget[0] as HTMLInputElement).value
     const params = new URLSearchParams(window.location.search)
     params.set('search', value)
     router.push(`${window.location.pathname}?${params}`)
